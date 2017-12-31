090388d670d7fa907b185a796f80a074a8225e8b
Stephen Shilale <sshilal1@binghamton.edu>
2017-12-30 21:33:59 -0500

    added basic login page

---
+++ components/LoginInfo.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+export default class LoginInfo extends React.Component {
+
+	constructor(props) {
+    super(props);
+    this.state = {
+			user : '',
+			password : '',
+			confirm : '',
+			registering : false
+		}
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.signUp = this.signUp.bind(this);
+    this.register = this.register.bind(this);
+  }
+
+	handleInputChange(event) {
+		const target = event.target;
+
+		this.setState({
+			[target.name] : target.value
+		})
+	}
+
+	handleSubmit(event) {
+		var entry = Object.assign({}, this.state, {});
+    this.props.onAdd(entry);
+  }
+
+  signUp() {
+  	this.setState({
+  		registering : true
+  	})
+  }
+
+  register() {
+  	this.setState({
+  		registering : false
+  	})
+  }
+
+	render() {
+
+		var style = {
+			padding: "10px",
+			margin: "5px",
+			marginLeft: 0,
+			paddingLeft: 0,
+			display: "inline-block",
+			fontSize: "2rem",
+			flex: 1
+		}
+		var inputStyle = {
+			padding: "10px",
+			margin: "5px",
+			display: "inline-block",
+			fontSize: "1rem",
+			flex: 1
+		}
+
+		if (this.state.registering) {
+			return (
+				<div style={{width:"75%", display:"flex", flexDirection:"column", paddingTop:"100px"}}>
+						<div style={{display:"flex"}}>
+							<label style={style}>User Name</label>
+							<input style={inputStyle} name="user" onChange={this.handleInputChange}/>
+						</div><div style={{display:"flex"}}>
+							<label style={style}>Password</label>
+							<input style={inputStyle} name="password" onChange={this.handleInputChange}/>
+						</div><div style={{display:"flex"}}>
+							<label style={style}>Confirm Password</label>
+							<input style={inputStyle} name="confirm" onChange={this.handleInputChange}/>
+						</div>
+						<button onClick={this.register} style={style} type="submit" label="Create User">Register</button>
+				</div>
+			)
+		}
+
+		else {
+			return (
+				<div style={{width:"75%", display:"flex", flexDirection:"column", paddingTop:"100px"}}>
+					<div style={{display:"flex"}}>
+						<label style={style}>User Name</label>
+						<input style={inputStyle} name="user" onChange={this.handleInputChange}/>
+					</div><div style={{display:"flex"}}>
+						<label style={style}>Password</label>
+						<input style={inputStyle} name="password" onChange={this.handleInputChange}/>
+					</div>
+					<button onClick={this.handleSubmit} style={style} type="submit" label="Create User">Log In</button>
+					<div style={{display:"flex"}} onClick={this.signUp}>
+						Not Registered? Sign up!
+					</div>
+				</div>
+			)
+		}
+	}
+}
